3ae4031331c4874048e1e33ea79f67e659e6bdcb
Guest User <Guest@Epicodus-2E.local>
2017-12-17 16:18:43 -0800

    edited index.html scripts.js styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
+
 // back-end logic
-function
+function Order(style, bread, size, quantity) {
+   this.piStyle = style;
+   this.piBread = bread;
+   this.piSize = size;
+   this.piQuantity = quantity;
+   this.piToppings = 0;
+   this.piToppingNames = [];
+}
 
+Order.prototype.totalPrice = function() {
+   var total = '$' + ((this.piStyle + this.piBread + this.piSize + this.piToppings) * this.piQuantity);
+   return total;
+};
 
+ Order.prototype.addTopping = function(value, name) {
+  this.piToppings += value;
+  this.piToppingNames.push(name);
+};
+
+ Order.prototype.toppingDescription = function() {
+   return this.piToppingNames.join(', ');
+}
 
 
 
