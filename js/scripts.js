fe0f21c479c1c0d4314c8c902d43c6fd35a26452
1WayOrAnother <youneedit2@gmail.com>
2017-12-18 07:47:56 -0800

    last changes to index.html styles.css scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,59 @@
-
 // back-end logic
 function Order(style, bread, size, quantity) {
-   this.piStyle = style;
-   this.piBread = bread;
-   this.piSize = size;
-   this.piQuantity = quantity;
-   this.piToppings = 0;
-   this.piToppingNames = [];
+  this.piStyle = style;
+  this.piBread = bread;
+  this.piSize = size;
+  this.piQuantity = quantity;
+  this.piToppings = 0;
+  this.piToppingNames = [];
 }
 
 Order.prototype.totalPrice = function() {
-   var total = '$' + ((this.piStyle + this.piBread + this.piSize + this.piToppings) * this.piQuantity);
-   return total;
+  var total = '$' + ((this.piStyle + this.piBread + this.piSize + this.piToppings) * this.piQuantity);
+  return total;
 };
 
- Order.prototype.addTopping = function(value, name) {
+Order.prototype.addTopping = function(value, name) {
   this.piToppings += value;
   this.piToppingNames.push(name);
 };
 
- Order.prototype.toppingDescription = function() {
-   return this.piToppingNames.join(', ');
+Order.prototype.toppingDescription = function() {
+  return this.piToppingNames.join(', ');
 }
 
 // front-end logic
 $(document).ready(function() {
 
-$("form").submit(function(event) {
+  $("form").submit(function(event) {
     event.preventDefault();
     $("ul#receipt").empty(newOrder);
     $('#container1').slideUp();
     $('#container2').show(1000);
 
+    var inputtedStyle = parseFloat($('#piStyle').val());
+    var inputtedBread = parseFloat($('#piBread').val());
+    var inputtedSize = parseFloat($('#piSize').val());
+    var inputtedQuantity = parseFloat($('#piQuantity').val());
 
-  var inputtedStyle = parseFloat($('#piStyle').val());
-  var inputtedBread = parseFloat($('#piBread').val());
-  var inputtedSize = parseFloat($('#piSize').val());
-  var inputtedQuantity = parseFloat($('#piQuantity').val());
+    var cartStyle = $('#piStyle option:selected').text();
+    var cartBread = $('#piBread option:selected').text();
+    var cartSize = $('#piSize option:selected').text();
+    var cartQuantity = $('#piQuantity option:selected').text();
 
+    var newOrder = new Order(inputtedStyle, inputtedBread, inputtedSize, inputtedQuantity);
 
-  var cartStyle = $('#piStyle option:selected').text();
-  var cartBread = $('#piBread option:selected').text();
-  var cartSize = $('#piSize option:selected').text();
-  var cartQuantity = $('#piQuantity option:selected').text();
+    $('input[type=checkbox]:checked').each(function(index, checkbox) {
+      newOrder.addTopping(parseFloat($(checkbox).val()), checkbox.name);
+    });
 
+    $('h4#receiptDescription').append('You ordered ' + cartQuantity + ' ' + cartSize + ' ' + cartBread + ' ' + cartStyle);
 
-  var newOrder = new Order(inputtedStyle, inputtedBread, inputtedSize, inputtedQuantity);
+    $('h4#receiptToppings').append('with toppings: ' + newOrder.toppingDescription() + '.');
 
-
-
-$('input[type=checkbox]:checked').each(function(index, checkbox) {
-  newOrder.addTopping(parseFloat($(checkbox).val()), checkbox.name);
+    $('h4#receiptTotal').append('Your total comes to ' + newOrder.totalPrice());
+  });
 });
-
-
-  $('h4#receiptDescription').append('You ordered ' + cartQuantity + ' ' + cartSize + ' ' + cartBread + ' ' + cartStyle);
-
-  $('h4#receiptToppings').append('with toppings: ' + newOrder.toppingDescription() + '.');
-
-
-  $('h4#receiptTotal').append('Your total comes to ' + newOrder.totalPrice());
-});
-});
-
-
-
-
-
-
-
-
-
-
-
 
 
 
